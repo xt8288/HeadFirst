2fbd8877effadd5f2024ac603c41a00b5deda860
Administrator <3196336929@qq.com>
2019-08-28 23:44:25 +0800

    列表切片1

---
+++ Marvin.py
@@ -0,0 +1,15 @@
+paranoid_android="Marvin"
+letters=list(paranoid_android)
+for char in letters:
+    print('\t',char,end='')
+
+# for a in range(10):
+#     print(a)
+# for a in range(10):
+#     print(a,end='')
+# for a in range(10):
+#     print(a,end='\t')
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print("%d*%d=%d\t"%(i,j,i*j),end="")
+#     print()
